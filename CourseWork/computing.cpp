8de0abd91379a5114898f432defc80abe20af9ef
ahecient <48700087+ahecient@users.noreply.github.com>
2019-05-18 21:44:35 +0300

    Добавьте файлы проекта.

---
+++ CourseWork/computing.cpp
@@ -0,0 +1,109 @@
+#include "computing.h"
+
+
+computing::~computing(){
+}
+
+void computing::fill_vec(std::vector<std::vector<long double>> &vec){
+	vec.resize(x_points_count + 1);
+
+	for (int i = 0; i < vec.size(); ++i){
+		vec.at(i).resize(t_points_count);
+		std::fill(vec.at(i).begin(), vec.at(i).end(), 0);
+	}
+}
+
+long double computing::precision_computing(const long double x, const long double t, const long double a, const long double A, const long double B, const long double C){
+	return	(pow((a * t + A), 3/4) * pow(((x + B) * (C * x + B * C + 1)), -3/2));
+}
+
+long double computing::approximate_computing(const long double w, const long double wleft, const long double wright, const long double r){
+	return (w - pow(w, (-4/3)) * r * (pow((wright - wleft), 2) * (1 / (3 * w)) - (wleft - 2 * w + wright)));
+}
+
+void computing::preparation(){
+	if (r > 0.5){
+		std::cerr << "ERROR. r =  " << r << std::endl;
+		getchar();
+		exit(EXIT_FAILURE);
+	}
+
+	fill_vec(approximate_parallel);
+	fill_vec(precision_parallel);
+	fill_vec(approximate_serial);
+	fill_vec(precision_serial);
+
+	for (int i = 0; i < x_points_count + 1; ++i){
+		approximate_parallel[i][0] = precision_computing(i * step_x, 0, a, A, B, C);
+		approximate_serial[i][0] = precision_computing(i * step_x, 0, a, A, B, C);
+
+	}
+
+	for (int j = 1; j < t_points_count; ++j){
+		approximate_parallel[0][j] = precision_computing(0, j * step_t, a, A, B, C);
+		approximate_parallel[x_points_count][j] = precision_computing(x_points_count * step_x, j * step_t, a, A, B, C);
+
+		approximate_serial[0][j] = precision_computing(0, j * step_t, a, A, B, C);
+		approximate_serial[x_points_count][j] = precision_computing(x_points_count * step_x, j * step_t, a, A, B, C);
+	}
+}
+
+void computing::work(const bool flag, std::vector<std::vector<long double>> &vec_approximate, std::vector<std::vector<long double>> &vec_precision){
+	int threads_num = 0;
+#pragma omp parallel if (flag)
+	{
+	for (int j = 1; j < t_points_count; ++j){
+#pragma omp for
+		for (int i = 1; i < x_points_count; ++i){
+			vec_approximate[i][j] = approximate_computing(vec_approximate[i][j - 1], vec_approximate[i - 1][j - 1], vec_approximate[i + 1][j - 1], r);
+			vec_precision[i][j] = precision_computing(i * step_x, j * step_t, a, A, B, C);
+		}
+	}
+	threads_num = omp_get_num_threads();
+	}
+	std::cout << "Number of threads: " << threads_num << std::endl;
+}
+
+long double computing::maximum(std::vector<std::vector<long double>> &vec){
+	long double  maximum = std::numeric_limits<long double>::min();
+	for (int i = 0; i < vec.size(); ++i){
+		long double temp_maximum = *std::max_element(vec.at(i).begin(), vec.at(i).end());
+		if (temp_maximum > maximum){
+			maximum = temp_maximum;
+		}
+	}
+	return  (maximum);
+}
+
+void computing::epsilon(std::vector<std::vector<long double>>&vec_absolute, std::vector<std::vector<long double>>&vec_relative){
+	for (int j = 1; j < t_points_count; ++j){
+		for (int i = 1; i < x_points_count; ++i){
+			vec_absolute[i][j] = fabs(precision_parallel[i][j] - approximate_parallel[i][j]);
+			vec_relative[i][j] = vec_absolute[i][j] / precision_parallel[i][j];
+		}
+	}
+	long double maximum_absolute = maximum(vec_absolute);
+	std::cout << "Absolute: " << maximum_absolute << std::endl;
+	long double maximum_relative = maximum(vec_relative);
+	std::cout << "Relative: " << maximum_relative * 100 << "%" << std::endl;
+}
+
+void computing::write(){
+	std::ofstream fout("results.txt");
+
+	for (int i = 0; i < x_points_count + 1; ++i){
+		for (int j = 0; j < t_points_count + 1; ++j){
+			fout << i << "\t" << j << "\t" << std::setprecision(17) << std::fixed << approximate_parallel[i][j] << std::endl;
+		}
+	}
+	std::cout << "finish writing in the file." << std::endl;
+}
+
+void computing::computing_epsilon(){
+	
+	fill_vec(absolute_parallel);
+	fill_vec(relative_parallel);
+	epsilon(absolute_parallel, relative_parallel);
+	
+	write();
+}
---
+++ CourseWork/computing.h
@@ -0,0 +1,51 @@
+#pragma once
+#include <algorithm>
+#include <vector>
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <omp.h>
+class computing{
+public:
+	computing() : length(1.0f), x_points_count(30), t_points_count(2000), a(1.0), A(2), B(0.2), C(0.2),
+
+		step_x(length / (x_points_count)), step_t(length / (t_points_count)),
+		error((step_t) / (step_x * step_x)), r(a * error) {}
+	~computing();
+
+	std::vector<std::vector<long double>> approximate_parallel;
+	std::vector<std::vector<long double>> precision_parallel;
+
+	std::vector<std::vector<long double>> approximate_serial;
+	std::vector<std::vector<long double>> precision_serial;
+
+	void preparation();
+	void work(const bool, std::vector<std::vector<long double>>&, std::vector<std::vector<long double>>&);
+	void computing_epsilon();
+	
+
+private:
+	const long double length;
+	const int x_points_count;
+	const int t_points_count;
+	const long double step_x;
+	const long double step_t;
+	const long double a;
+	const double long error;
+	const double long r;
+	const long double A;
+	const long double B;
+	const long double C;
+
+	std::vector<std::vector<long double>> absolute_parallel;
+	std::vector<std::vector<long double>> relative_parallel;
+
+	void fill_vec(std::vector<std::vector<long double>>&);
+	long double precision_computing(const long double x, const long double t, const long double a, const long double A, const long double B, const long double C);
+	long double approximate_computing(const long double w, const long double wleft, const long double wright, const long double r);
+	long double maximum(std::vector<std::vector<long double>> &);
+	void epsilon(std::vector<std::vector<long double>>&, std::vector<std::vector<long double>>&);
+	void write();
+};
+
+
